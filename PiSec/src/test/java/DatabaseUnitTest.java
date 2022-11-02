fd2f85b5cd8717ba37828ff677d78ba45023eeea
s2793199 <a.a.tenhave@student.utwente.nl>
2022-11-02 11:27:16 +0100

    added unit testing for alerts date

--- PiSec/src/test/java/DatabaseUnitTest.java
+++ PiSec/src/test/java/DatabaseUnitTest.java
@@ -24,8 +24,22 @@ public class DatabaseUnitTest {
         try {
             addAccount(account, password, salt);
         } catch (SQLException e) {
-            e.printStackTrace();
-            assertEquals("test12345", getAccount("test12345").getUsername());
+//            errorCode 19 stands for unique constraint failed
+            if (e.getErrorCode() != 19)
+                e.printStackTrace();
         }
+//        checks if account was added
+        assertEquals("test12345", getAccount("test12345").getUsername());
+    }
+
+    /**
+     * Test alert storage in database
+     */
+    @Test
+    public void testAlertDatabase() {
+        Long id = 0L;
+        Long date = 1666793754726L;
+//        checks if alert datetime file is stored correct
+        assertEquals(date, getAlert(id).getDateTime().getTime());
     }
 }
