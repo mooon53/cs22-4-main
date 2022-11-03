7160552d06fbce1f47c5b859b9cb2b537215f3d0
Thijs Frauenfelder <thijsfrauenfelder@icloud.com>
2022-11-04 00:59:56 +0100

    testing

--- PiSec/src/main/webapp/javaScript/test.js
+++ PiSec/src/main/webapp/javaScript/test.js
@@ -1,12 +1,15 @@
 function onLoad() {
-	let request = makeRequest("GET", "rest/alerts/1", true)
+	const url = new URL(location.href);
+	let id = 1;
+	if (url.searchParams && url.searchParams.get("id")) {
+		id = url.searchParams.get("id");
+	}
+	let request = makeRequest("GET", "rest/alerts/" + id, true)
 	request.onreadystatechange = function() {
 		if (this.readyState === 4 && this.status === 200) {
 			const response = JSON.parse(this.responseText);
-			document.getElementById("body").innerHTML = `<img src="data:image/jpg;base64, ${response.image}">`
+			document.getElementById("body").innerHTML = `<video controls src="data:video/mp4;base64, ${response.image}"/>`;
 		}
 	}
 	request.send();
 }
-
-	Math.max([0, 3 ,1 , 4])
