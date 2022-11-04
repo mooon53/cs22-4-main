1fba213c8279a16be6134dc296cc0cdba6db4826
Thijs Frauenfelder <thijsfrauenfelder@icloud.com>
2022-11-04 01:09:43 +0100

    updated setup scripts

--- PiSec/src/main/shell/install.sh
+++ PiSec/src/main/shell/install.sh
@@ -1,4 +1,4 @@
-sudo apt install authbind sqlite3 raspberrypi-kernel-headers openjdk-11-jdk-headless zip iftop atop iotop nmap netcat vim screen libncurses5-dev build-essential fakeroot rsync git flex bison libssl-dev
+sudo apt install authbind sqlite3 openjdk-11-jdk-headless
 sudo apt update
 sudo apt upgrade
 curl -LJO -H 'Accept: application/octet-stream' https://github.com/WiringPi/WiringPi/releases/download/2.61-1/wiringpi-2.61-1-arm64.deb
--- PiSec/src/main/shell/startup.sh
+++ PiSec/src/main/shell/startup.sh
@@ -1,3 +1,5 @@
+# from laptop: scp -r <tomcat home folder> <user>@<pi host name>:tomcat/
+# from laptop: scp -r <Project folder>/src/main/python <user>@<pi host name>:
 cd ~/tomcat/apache-tomcat-10.1.1
 authbind --deep ./bin/startup.sh
 cd ~/python
